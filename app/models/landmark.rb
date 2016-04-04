704dc9fd7b9ee31f4bd43747a79dbe8cb74d6e75
salisbum <megan.a.salisbury@gmail.com>
2016-04-04 16:46:20 -0400

    Styling changes per Hound, updated User and Landmark models in prep for dev work tomorrow

--- app/models/landmark.rb
+++ app/models/landmark.rb
@@ -1,8 +1,3 @@
 class Landmark < ActiveRecord::Base
   belongs_to :user
-
-  validates :name, presence: true
-  validates :location, presence: true
-  validates :image, presence: true
-  validates :description, presence: true, length: { minimum: 50 }  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,3 @@
 class User < ActiveRecord::Base
   has_many :landmarks
-
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :username, presence: true
-  validates :avatar_url, presence: true
 end

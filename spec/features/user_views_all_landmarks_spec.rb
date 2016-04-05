2236cc5f7f28e078337843410487e605d133a95a
Timothy Craig <ttcraig@Timothys-MacBook-Pro.local>
2016-04-05 14:36:05 -0400

    add feature landmark index

--- spec/features/user_views_all_landmarks_spec.rb
+++ spec/features/user_views_all_landmarks_spec.rb
@@ -5,17 +5,17 @@ require 'rails_helper'
 feature "user sees a list of landmarks" do
   scenario "[success!] sees a list of landmarks in alphabetical order" do
     statue_of_liberty = Landmark.create(
-    name: "Statue of Liberty",
-    location: "New York",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Statue_of_Liberty_7.jpg/500px-Statue_of_Liberty_7.jpg",
-    description: "A colossal neoclassical sculpture on Liberty Island"
+      name: "Statue of Liberty",
+      location: "New York",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Statue_of_Liberty_7.jpg/500px-Statue_of_Liberty_7.jpg",
+      description: "A colossal neoclassical sculpture on Liberty Island"
     )
 
     grand_canyon = Landmark.create(
-    name: "Grand Canyon",
-    location: "Arizona",
-    image: "https://lh6.googleusercontent.com/-0IU2MaN1t0c/AAAAAAAAAAI/AAAAAAAAABs/ROeqMxHHRfY/s0-c-k-no-ns/photo.jpg",
-    description: "A colossal neoclassical sculpture on Liberty Island"
+      name: "Grand Canyon",
+      location: "Arizona",
+      image: "https://lh6.googleusercontent.com/-0IU2MaN1t0c/AAAAAAAAAAI/AAAAAAAAABs/ROeqMxHHRfY/s0-c-k-no-ns/photo.jpg",
+      description: "A colossal neoclassical sculpture on Liberty Island"
     )
 
     visit landmarks_path

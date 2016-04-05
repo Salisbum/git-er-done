5c1a6f6ca0553ccea99356cad05e55fda6b55a80
Timothy Craig <ttcraig@Timothys-MacBook-Pro.local>
2016-04-05 14:18:39 -0400

    add feature landmark index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160404195736) do
+ActiveRecord::Schema.define(version: 20160405145325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 20160404195736) do
     t.text     "description", null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "user_id",     null: false
   end
 
   create_table "users", force: :cascade do |t|

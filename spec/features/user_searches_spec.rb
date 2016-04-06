3452f559b5b70dac249a8e6a1dd823fb684b7b39
Paul <paul@Gregs-MacBook-Pro.local>
2016-04-06 16:51:54 -0400

    hound fixes

--- spec/features/user_searches_spec.rb
+++ spec/features/user_searches_spec.rb
@@ -3,16 +3,18 @@ feature "User searches for a landmark" do
 
   before(:each) do
     @statue_of_liberty = Landmark.create(
-    name: "Statue of Liberty",
-    location: "New York",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Statue_of_Liberty_7.jpg/500px-Statue_of_Liberty_7.jpg",
-    description: "A colossal neoclassical sculpture on Liberty Island")
+      name: "Statue of Liberty",
+      location: "New York",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Statue_of_Liberty_7.jpg/500px-Statue_of_Liberty_7.jpg",
+      description: "A colossal neoclassical sculpture on Liberty Island"
+    )
 
     @grand_canyon = Landmark.create(
-    name: "Grand Canyon",
-    location: "Arizona",
-    image: "https://lh6.googleusercontent.com/-0IU2MaN1t0c/AAAAAAAAAAI/AAAAAAAAABs/ROeqMxHHRfY/s0-c-k-no-ns/photo.jpg",
-    description: "A colossal neoclassical sculpture on Liberty Island")
+      name: "Grand Canyon",
+      location: "Arizona",
+      image: "https://lh6.googleusercontent.com/-0IU2MaN1t0c/AAAAAAAAAAI/AAAAAAAAABs/ROeqMxHHRfY/s0-c-k-no-ns/photo.jpg",
+      description: "A colossal neoclassical sculpture on Liberty Island"
+    )
   end
 
   scenario "User finds said landmark" do

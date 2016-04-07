4de4e52a3605b86bd0494cfc614de4429e281af2
Anna Guidi <guidi.a@husky.neu.edu>
2016-04-07 15:32:43 -0400

    add user edit profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,4 +3,29 @@ class ProfilesController < ApplicationController
     @profile = Profile.find(params[:id])
     @user = @profile.user
   end
+
+  def edit
+    @profile = Profile.find(params[:id])
+  end
+
+  def update
+    @profile = Profile.find(params[:id])
+    if @profile.update(profile_params)
+      flash[:notice] = "Successfully edited your Profile!"
+      redirect_to profile_path(@profile)
+    else
+      flash[:error] = "Did not manage to update profile. #{@profile.errors.full_messages.join(', ')}."
+      render :edit
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(
+      :username,
+      :location,
+      :pun
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,4 @@ Rails.application.routes.draw do
   devise_for :users
   resources :landmarks
   resources :profiles, except: [:index]
-
-  get '/profiles/edit', to: redirect('/users/edit')
 end

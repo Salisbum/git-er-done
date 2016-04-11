7a88f8f87ec2eefcb61c564909d57834645240bc
Timothy Craig <ttcraig@Timothys-MacBook-Pro.local>
2016-04-11 16:55:40 -0400

    add delete an account

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,4 +7,16 @@ class ProfilesController < ApplicationController
   def index
     @profiles = Profile.all
   end
+
+  def destroy
+    profile = Profile.find(params[:id])
+    user = profile.user
+
+    if profile.destroy && user.destroy
+      flash[:notice] = "Account Deleted Successfully"
+    else
+      flash[:error] = "Account Not Deleted"
+    end
+    render :index
+  end
 end
--- spec/features/admin_deletes_a_user_spec.rb
+++ spec/features/admin_deletes_a_user_spec.rb
@@ -2,9 +2,8 @@ require 'rails_helper'
 
 feature "Admin visits users index" do
   let!(:user1) { FactoryGirl.create(:user) }
-  let!(:user2) { FactoryGirl.create(:user) }
 
-  scenario "Admin visits users index sucessfully" do
+  scenario "Admin deletes user sucessfully" do
 
     profile1 = Profile.create(
       username: "Xander321",
@@ -14,18 +13,13 @@ feature "Admin visits users index" do
       user: user1
     )
 
-    profile2 = Profile.create(
-    username: "Xander123",
-    location: "Boston",
-    avatar_url: "https://s-media-cache-ak0.pinimg.com/236x/50/bb/11/50bb1149e480e46d721d8a813a8ef3d4.jpg",
-    pun: "Isn't this punny?",
-    user: user2
-    )
-
     user_login
     visit profiles_path
-
+    
     expect(page).to have_content user1.email
-    expect(page).to have_content user2.email
+
+    click_on "Delete"
+
+    expect(page).to_not have_content user1.email
   end
 end

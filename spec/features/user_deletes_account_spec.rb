d75952f6f15d172aff4032f920d8d3d3f5e73e7e
Timothy Craig <ttcraig@Timothys-MacBook-Pro.local>
2016-04-13 15:11:39 -0400

    refactored test to use FG; fixed styling

--- spec/features/user_deletes_account_spec.rb
+++ spec/features/user_deletes_account_spec.rb
@@ -1,49 +1,30 @@
 require 'rails_helper'
 feature "User deletes account" do
   let!(:user) { FactoryGirl.create(:user) }
-
   scenario 'user navigates to edit account page' do
     user_login
     click_on 'Edit registration'
 
     expect(page).to have_content('Cancel my account')
   end
-  scenario 'user deletes account' do
-    user1 = User.create(
-      email: "tim@odsc.com",
-      password: "11223344"
-    )
+  scenario 'user deletes their own account, expects all reviews to be deleted' do
 
-    Profile.create(
-      username: "Xander321",
-      location: "Boston",
-      avatar_url: "https://s-media-cache-ak0.pinimg.com/236x/50/bb/11/50bb1149e480e46d721d8a813a8ef3d4.jpg",
-      pun: "Isn't this punny?",
-      user: user1
-    )
+    pyramid = FactoryGirl.create(:landmark, user: user)
 
-    pyramid = Landmark.create(
-      name: "Pyramids of Giza",
-      location: "Egypt",
-      image: "https://upload.wikimedia.org/wikipedia/commons/e/e3/Kheops-Pyramid.jpg",
-      description: "hot, but awesome.",
-      user: user1
-    )
-
-    Review.create(
-      body: "This place was kinda dope",
-      landmark: pyramid,
-      user: user1,
-      votes: "4"
-    )
+    review = FactoryGirl.create(:review, landmark: pyramid, user: user)
 
     visit root_path
+
     click_on 'Login'
-    fill_in 'Email', with: user1.email
-    fill_in 'Password', with: user1.password
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+
     click_on 'Log in'
+
     visit landmark_path(pyramid)
-    expect(page).to have_content "This place was kinda dope"
+
+    expect(page).to have_content review.body
 
     click_on 'Edit registration'
     click_on 'Cancel my account'
@@ -51,7 +32,9 @@ feature "User deletes account" do
     expect(page).to have_content('Sign Up')
 
     visit landmark_path(pyramid)
-    expect(page).to_not have_content "This place was kinda dope"
+
+    expect(page).to have_content("No reviews! Want to leave one?")
+    expect(page).to_not have_content review.body
   end
   scenario 'user attempts to log in with deleted account' do
     user_login

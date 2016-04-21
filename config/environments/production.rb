8a84c22eda1d103e2c4982169d0f1b78a3c5a358
salisbum <megan.a.salisbury@gmail.com>
2016-04-21 10:50:55 -0400

    foundation fixing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.serve_static_assets = true
 
   config.action_mailer.default_url_options = { host: 'http://landsnarkr.herokuapp.com' }
 

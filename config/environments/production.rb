4e6939ddea506929f89a4160a285a2915ff76cf6
salisbum <megan.a.salisbury@gmail.com>
2016-04-21 13:28:45 -0400

    removing static assets in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,6 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.serve_static_assets = true
 
   config.action_mailer.default_url_options = { host: 'http://landsnarkr.herokuapp.com' }
 
